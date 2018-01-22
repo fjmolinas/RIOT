e43aebdba3177bd0d8a82e0e45d22acd042f3ded
Francisco <femolina@uc.cl>
2018-01-22 14:59:35 -0300

    stm32_common/periph/uart: add baudrate setting function for uart

--- cpu/stm32_common/periph/uart.c
+++ cpu/stm32_common/periph/uart.c
@@ -134,6 +134,7 @@ int uart_init(uart_t uart, uint32_t baudrate, uart_rx_cb_t rx_cb, void *arg) {
 
     /* calculate and apply baudrate */
     if (periph_uart_set_baudrate(uart, baudrate) != UART_OK) {
+
         return UART_NOBAUD;
     }
 
