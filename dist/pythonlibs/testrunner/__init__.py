508b63fc7e3d3ce5d0174eec0b0fb8f123422872
Gaëtan Harter <gaetan.harter@fu-berlin.de>
2019-02-01 17:41:18 +0100

    testrunner: use riotnode inside
    Use 'riotnode' directly in '__init__.py' not with the 'spawn' wrapper.

--- dist/pythonlibs/testrunner/__init__.py
+++ dist/pythonlibs/testrunner/__init__.py
@@ -7,10 +7,11 @@
 # General Public License v2.1. See the file LICENSE in the top level
 # directory for more details.
 
-import os
 import sys
 from traceback import print_tb
+
 import pexpect
+import riotnode.node
 
 from .spawn import find_exc_origin, setup_child, teardown_child
 from .unittest import PexpectTestCase   # noqa, F401 expose to users
@@ -23,10 +24,12 @@ TIMEOUT = int(os.environ.get('RIOT_TEST_TIMEOUT') or 10)
 
 
 def run(testfunc, timeout=TIMEOUT, echo=True, traceback=False):
-    child = setup_child(timeout, env=os.environ,
-                        logfile=sys.stdout if echo else None)
+    node = riotnode.node.RIOTNode()
+    node.start_term(timeout=timeout, logfile=sys.stdout if echo else None)
+    node.reset()
+
     try:
-        testfunc(child)
+        testfunc(node.term)
     except pexpect.TIMEOUT:
         trace = find_exc_origin(sys.exc_info()[2])
         print("Timeout in expect script at \"%s\" (%s:%d)" % trace)
@@ -42,5 +45,5 @@ def run(testfunc, timeout=TIMEOUT, echo=True, traceback=False):
         return 1
     finally:
         print("")
-        teardown_child(child)
+        node.stop_term()
     return 0
