9f333c92874f640771d30ee2ee2c20945f59e288
francisco <femolina@uc.cl>
2019-03-05 12:00:58 +0100

    boards/im880b: provide initial support for im880b

---
+++ boards/im880b/include/board.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2019 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     boards_im880b
+ * @{
+ *
+ * @file
+ * @brief       Board specific definitions for the im808b board.
+ *
+ * @author      Francisco Molina <francois-xavier.molina@inria.fr>
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <stdint.h>
+
+#include "cpu.h"
+#include "periph_conf.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief   Initialize board specific hardware, including clock, LEDs and std-IO
+ */
+void board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BOARD_H */
+/** @} */
