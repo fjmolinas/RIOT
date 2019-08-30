da216bc56354c0ac0826bd693e59aa5f30256ab0
Juan Carrano <j.carrano@fu-berlin.de>
2019-08-30 16:04:13 +0200

    makefiles: replace $(shell echo..) by $(info..) $(warning..)
    Avoid calling the shell where there is no need. Saves time, prervents weird
    behavior with quotes and avoid the need for outout redirection, since the
    $(warning) already goes to stdout, in addition to printing the source of the
    message.

--- makefiles/docker.inc.mk
+++ makefiles/docker.inc.mk
@@ -282,7 +282,7 @@ DOCKER_VOLUMES_AND_ENV += $(if $(_is_git_worktree),$(call docker_volume,$(GIT_WO
 # The `flash`, `term`, `debugserver` etc. targets usually require access to
 # hardware which may not be reachable from inside the container.
 ..in-docker-container:
-	@echo '$(COLOR_GREEN)Launching build container using image "$(DOCKER_IMAGE)".$(COLOR_RESET)'
+	$(info $(COLOR_GREEN)Launching build container using image "$(DOCKER_IMAGE)".$(COLOR_RESET))
 	@# HACK: Handle directory creation here until it is provided globally
 	$(Q)mkdir -p $(BUILD_DIR)
 	$(DOCKER) run $(DOCKER_FLAGS) -t -u "$$(id -u)" \
