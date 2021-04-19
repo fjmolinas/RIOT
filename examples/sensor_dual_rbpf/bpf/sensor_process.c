73bbe91b7dbbf66e368aa9b3fe2d85ba1b21544b
Koen Zandberg <koen@bergzand.net>
2021-04-19 11:27:57 +0200

    Initial dual sensor example commit
    Very WIP

---
+++ examples/sensor_dual_rbpf/bpf/sensor_process.c
@@ -0,0 +1,55 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "bpf/bpfapi/helpers.h"
+
+#define SHARED_KEY 0x50
+#define AVERAGING_LEN 10
+#define PERIOD_US (1000 * 1000)
+
+static uint32_t _average(uint32_t *values)
+{
+    uint64_t sum = 0;
+    for (size_t i = 0; i < AVERAGING_LEN; i++) {
+        sum += values[i];
+    }
+    return sum / AVERAGING_LEN;
+}
+
+int periodic_incr(void *conf)
+{
+    uint32_t last_wakeup = bpf_ztimer_now();
+    uint32_t counter = 0;
+    size_t pos = 0;
+    bool initial = true;
+
+    uint32_t values[AVERAGING_LEN];
+
+    while (1) {
+        /* Read sensor value from sensor */
+        uint64_t value = 0;
+
+        if (initial) {
+            /* Fill array with the initial measurement */
+            for (size_t i = 0; i < AVERAGING_LEN; i++) {
+                values[i] = value;
+            }
+            initial = false;
+        }
+        else {
+            values[pos] = value;
+            pos++;
+            if (pos >= AVERAGING_LEN) {
+                pos = 0;
+            }
+        }
+
+        uint32_t average = _average(values);
+
+        bpf_store_global(SHARED_KEY, average);
+
+        bpf_ztimer_periodic_wakeup(&last_wakeup, PERIOD_US);
+    }
+
+    return 0;
+}
+
