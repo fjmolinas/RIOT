4be8252395cbdc2f4862b6475deb6cf637565aad
Francisco Molina <femolina@uc.cl>
2022-03-07 09:07:46 +0100

    boards/feather-nrf52840: add screen definitions

--- boards/feather-nrf52840/include/board.h
+++ boards/feather-nrf52840/include/board.h
@@ -57,6 +57,65 @@ extern "C" {
 #define BTN0_MODE           GPIO_IN_PU
 /** @} */
 
+/**
+ * @name    Configuration for ST7735 feather BOARD
+ * @{
+ */
+#define ST7735_PARAM_CS                 GPIO_PIN(0, 7)
+#define ST7735_PARAM_DCX                GPIO_PIN(1, 8)
+#define ST7735_PARAM_BL                 GPIO_PIN(0, 26)
+#define ST7735_PARAM_INVERTED           1
+#define ST7735_PARAM_NUM_LINES          160U
+#define ST7735_PARAM_RGB_CHANNELS       80U
+#define ST7735_OFFSET_X                 1
+#define ST7735_OFFSET_Y                 26
+#if IS_USED(MODULE_ST7735)
+#define BACKLIGHT_ON                    gpio_set(ST7735_PARAM_BL)
+#define BACKLIGHT_OFF                   gpio_clear(ST7735_PARAM_BL)
+#endif
+
+/**
+ * @brief   Card detect pin
+ */
+#define CARD_DETECT_PIN              (GPIO_PIN(0, 6))       /**< D11 */
+
+/**
+ * @name    Configuration for ILI9341 feather BOARD
+ * @{
+ */
+#define ILI9341_PARAM_CS             GPIO_PIN(0, 26)
+#define ILI9341_PARAM_DCX            GPIO_PIN(0, 27)
+
+/**
+ * @name    Configuration for STMPE811 feather BOARD
+ * @{
+ */
+#define STMPE811_PARAM_INT_PIN       GPIO_PIN(0, 8)         /**< D12 */
+#define STMPE811_PARAM_CS            GPIO_PIN(0, 7)         /**< D06 */
+
+/**
+ * @name    Backlight control defines, default uses LCD_BACKLIGHT_LOW values
+ * @{
+ *
+ */
+#if IS_USED(MODULE_ILI9341) || IS_USED(MODULE_STMPE811)
+#define BACKLIGHT_PIN          GPIO_PIN(1, 9)                       /**< Backlight pin, D13*/
+#define BACKLIGHT_ON           (gpio_set(BACKLIGHT_PIN))            /**< Turn backlight on */
+#define BACKLIGHT_OFF          (gpio_clear(BACKLIGHT_PIN))          /**< Turn backlight off */
+#endif
+/** @ */
+
+/**
+ * @name Onboard micro-sd slot pin definitions
+ * @{
+ */
+#define SDCARD_SPI_PARAM_SPI       SPI_DEV(0)
+#define SDCARD_SPI_PARAM_CS        GPIO_PIN(1, 8)
+#define SDCARD_SPI_PARAM_CLK       GPIO_PIN(0, 14)
+#define SDCARD_SPI_PARAM_MOSI      GPIO_PIN(0, 13)
+#define SDCARD_SPI_PARAM_MISO      GPIO_PIN(0, 15)
+/** @} */
+
 #ifdef __cplusplus
 }
 #endif
