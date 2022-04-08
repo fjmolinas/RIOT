48f978c813485c45ad849d66000c0e2ef2a477b5
Francisco Molina <femolina@uc.cl>
2022-04-08 17:10:14 +0200

    REMOVEME: tests/nanocoap_cli/Makefile: add CSP

--- tests/nanocoap_cli/main.c
+++ tests/nanocoap_cli/main.c
@@ -30,84 +30,13 @@ static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 extern int nanotest_client_cmd(int argc, char **argv);
 extern int nanotest_server_cmd(int argc, char **argv);
-static int _list_all_inet6(int argc, char **argv);
 
 static const shell_command_t shell_commands[] = {
     { "client", "CoAP client", nanotest_client_cmd },
     { "server", "CoAP server", nanotest_server_cmd },
-    { "inet6", "IPv6 addresses", _list_all_inet6 },
     { NULL, NULL, NULL }
 };
 
-/* _list_all_inet6() and _print_addr() derived from sc_gnrc_netif.c */
-
-static void _print_addr(ipv6_addr_t *addr, uint8_t flags)
-{
-    char addr_str[IPV6_ADDR_MAX_STR_LEN];
-
-    printf("          inet6 addr: ");
-    ipv6_addr_to_str(addr_str, addr, sizeof(addr_str));
-    printf("%s  scope: ", addr_str);
-    if ((ipv6_addr_is_link_local(addr))) {
-        printf("link");
-    }
-    else {
-        printf("global");
-    }
-    if (flags & GNRC_NETIF_IPV6_ADDRS_FLAGS_ANYCAST) {
-        printf(" [anycast]");
-    }
-    if (flags & GNRC_NETIF_IPV6_ADDRS_FLAGS_STATE_TENTATIVE) {
-        printf("  TNT[%u]",
-               flags & GNRC_NETIF_IPV6_ADDRS_FLAGS_STATE_TENTATIVE);
-    }
-    else {
-        switch (flags & GNRC_NETIF_IPV6_ADDRS_FLAGS_STATE_MASK) {
-            case GNRC_NETIF_IPV6_ADDRS_FLAGS_STATE_DEPRECATED:
-                printf("  DPR");
-                break;
-            case GNRC_NETIF_IPV6_ADDRS_FLAGS_STATE_VALID:
-                printf("  VAL");
-                break;
-            default:
-                printf("  UNK");
-                break;
-        }
-    }
-    printf("\n");
-}
-
-static int _list_all_inet6(int argc, char **argv)
-{
-    (void)argc;
-    (void)argv;
-    gnrc_netif_t *netif = NULL;
-    unsigned addr_qty = 0;
-
-    while ((netif = gnrc_netif_iter(netif))) {
-        ipv6_addr_t ipv6_addrs[CONFIG_GNRC_NETIF_IPV6_ADDRS_NUMOF];
-
-        int res = gnrc_netapi_get(netif->pid, NETOPT_IPV6_ADDR, 0, ipv6_addrs,
-                                  sizeof(ipv6_addrs));
-        if (res >= 0) {
-            uint8_t ipv6_addrs_flags[CONFIG_GNRC_NETIF_IPV6_ADDRS_NUMOF];
-
-            memset(ipv6_addrs_flags, 0, sizeof(ipv6_addrs_flags));
-            /* assume it to succeed (otherwise array will stay 0) */
-            gnrc_netapi_get(netif->pid, NETOPT_IPV6_ADDR_FLAGS, 0,
-                            ipv6_addrs_flags, sizeof(ipv6_addrs_flags));
-            /* yes, the res of NETOPT_IPV6_ADDR is meant to be here ;-) */
-            for (unsigned i = 0; i < (res / sizeof(ipv6_addr_t)); i++) {
-                _print_addr(&ipv6_addrs[i], ipv6_addrs_flags[i]);
-                addr_qty++;
-            }
-        }
-    }
-    if (!addr_qty) {
-        printf("no addresses found\n");
-    }
-    return 0;
-}
 
 int main(void)
 {
