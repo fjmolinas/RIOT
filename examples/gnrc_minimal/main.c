51b51864dc80a53665ff08d0ed082216f4dd2c92
Francisco Molina <femolina@uc.cl>
2022-04-14 18:26:21 +0200

    FIXME: examples/gnrc_minimal: nanocoap minimal

--- examples/gnrc_minimal/main.c
+++ examples/gnrc_minimal/main.c
@@ -24,30 +24,32 @@
 #include "net/ipv6/addr.h"
 #include "net/gnrc.h"
 #include "net/gnrc/netif.h"
+#include "net/nanocoap_sock.h"
+
+#define COAP_INBUF_SIZE (256U)
+
+/* Extend stacksize of nanocoap server thread */
+#define NANOCOAP_SERVER_QUEUE_SIZE     (2)
+static msg_t _nanocoap_server_msg_queue[NANOCOAP_SERVER_QUEUE_SIZE];
+
+
+/* must be sorted by path (ASCII order) */
+const coap_resource_t coap_resources[] = {
+    COAP_WELL_KNOWN_CORE_DEFAULT_HANDLER,
+};
+
+const unsigned coap_resources_numof = ARRAY_SIZE(coap_resources);
+
 
 int main(void)
 {
+    /* nanocoap_server uses gnrc sock which uses gnrc which needs a msg queue */
+    msg_init_queue(_nanocoap_server_msg_queue, NANOCOAP_SERVER_QUEUE_SIZE);
 
-    puts("RIOT network stack example application");
+    /* initialize nanocoap server instance */
+    uint8_t buf[COAP_INBUF_SIZE];
+    sock_udp_ep_t local = { .port=COAP_PORT, .family=AF_INET6 };
+    nanocoap_server(&local, buf, sizeof(buf));
 
-    /* get interfaces and print their addresses */
-    gnrc_netif_t *netif = NULL;
-    while ((netif = gnrc_netif_iter(netif))) {
-        ipv6_addr_t ipv6_addrs[CONFIG_GNRC_NETIF_IPV6_ADDRS_NUMOF];
-        int res = gnrc_netapi_get(netif->pid, NETOPT_IPV6_ADDR, 0, ipv6_addrs,
-                                  sizeof(ipv6_addrs));
-
-        if (res < 0) {
-            continue;
-        }
-        for (unsigned i = 0; i < (unsigned)(res / sizeof(ipv6_addr_t)); i++) {
-            char ipv6_addr[IPV6_ADDR_MAX_STR_LEN];
-
-            ipv6_addr_to_str(ipv6_addr, &ipv6_addrs[i], IPV6_ADDR_MAX_STR_LEN);
-            printf("My address is %s\n", ipv6_addr);
-        }
-    }
-
-    /* main thread exits */
     return 0;
 }
